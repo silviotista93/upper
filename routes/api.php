91c83acca51016e79bd9f43c91fffbe5054055be
Silvio <silviotista93@gmail.com>
2019-05-13 11:35:33 -0500

    ruta

--- routes/api.php
+++ routes/api.php
@@ -35,7 +35,7 @@ Route::group(['prefix' => 'auth', 'namespace'=>'Movil'], function () {
     });
 });
 
-Route::group(['middleware' => 'auth:api','prefix' => 'profile', 'namespace'=>'Movil'], function () {
+Route::group(['prefix' => 'profile', 'namespace'=>'Movil'], function () {
     Route::post('update', 'UserController@update');
     Route::post('update-password', 'UserController@updatePassword');
 });
